dd69ddd7d6df1863b9fa6949a9557d0c6e7fdc40
punamkapooraolcom <punam.kapoor@aol.com>
2025-05-30 13:16:03 +0300

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>🚀 Rozgaar Bazaar is Live with Next.js!</h1>
+      <p>This app is deployed via Firebase App Hosting and GitHub Actions.</p>
+    </div>
+  );
+}
