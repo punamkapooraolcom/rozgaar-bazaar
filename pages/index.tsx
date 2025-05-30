e9868737eb762f3b42b74b9818ac136e7f1cfc03
punamkapooraolcom <punam.kapoor@aol.com>
2025-05-30 18:02:57 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Home() {
   return (
     <div style={{ textAlign: "center", padding: "40px", fontFamily: "sans-serif" }}>
-      <h1>🚀 Rozgaar Bazaar is Live with Next.js!</h1>
+      <h1>🚀 Rozgaar Bazaar is Live with Next.js! Hellow & Good morning</h1>
       <p>This app is deployed via Firebase App Hosting and GitHub Actions.</p>
     </div>
   );
